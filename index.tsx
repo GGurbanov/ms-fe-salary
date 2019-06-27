2cd5306b619cf2d743811c4e5afe14e3b585785d
Gurban Gurbanov <gurban.gurbanov@pashabank.az>
2019-06-27 16:01:47 +0400

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SalaryModule: React.FC = () => {
+    return <div className="container">
+        <div className="row">
+            <div className="col-sm" style={{ marginTop: "20px" }}>
+                Salary module
+            </div>
+        </div>
+    </div>
+};
+
+export default SalaryModule;
