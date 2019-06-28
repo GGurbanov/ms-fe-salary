091704379be798fe1c5bbd45dc05e5d50589319e
Gurban Gurbanov <gurban.gurbanov@pashabank.az>
2019-06-28 12:20:04 +0400

    Added jumbotron

--- index.tsx
+++ index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import {
+    Button,
+    Jumbotron
+} from 'reactstrap';
+
 const SalaryModule: React.FC = () => {
     return <div className="container">
         <div className="row">
-            <div className="col-sm" style={{ marginTop: "20px" }}>
-                Salary module
+            <div className="col-sm">
+                <div style={{ marginTop: "20px" }}>
+                    <Jumbotron>
+                        <h1 className="display-3">Welcome to Salary module!</h1>
+                        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <hr className="my-2" />
+                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                        <p className="lead">
+                            <Button color="primary">Learn More</Button>
+                        </p>
+                    </Jumbotron>
+                </div>
             </div>
         </div>
     </div>
